fcb1ada1bd3608ee88b4012cb4dbdd104a03b2a4
Anselmo5 <anselmohenrique191@gmail.com>
2023-08-29 21:38:39 -0300

    alteração

--- Project/src/components/form.jsx
+++ Project/src/components/form.jsx
@@ -3,11 +3,37 @@ import './form.css'
 import footerexport from '../assets/Group 5946.png'
 const form = () => {
   return (
-    <div className='aling'>
+    <div>
 
           <div className='alingimg'>
               <img src={footerexport} alt="" />
           </div>
+
+          <div className='aling'>
+                <form className=' forms'>
+                    <h2 className='texformats'>Crie sua conta </h2>
+                      <div className='faixa'>
+                        <label htmlFor="nome" className='texfaixa'> Seu Nome</label>
+                        <input type="text" name='nome'  className='inpfaixa'/>
+                      </div>
+
+                      <div className='faixa'>
+                        <label htmlFor="nome" className='texfaixa'> Seu Nome</label>
+                        <input type="email" name='nome'  className='inpfaixa'/>
+                      </div>
+
+                      <div className='faixa'>
+                        <label htmlFor="nome" className='texfaixa'> Seu Nome</label>
+                        <input type="password" name='nome'  className='inpfaixa'/>
+                      </div>
+
+                      <div className='btnfaixa'>
+                                                                            
+                        <input type="submit" name='nome' className='btnfaixa'/>
+                      </div>
+                </form>
+          </div>
+          
     </div>
     
   )
