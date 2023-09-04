5da7fb6b87d75f583ccdd353746b923dab9908bf
Anselmo5 <anselmohenrique191@gmail.com>
2023-09-04 18:27:42 -0300

    alteração

--- Project/src/components/form.jsx
+++ Project/src/components/form.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 import './form.css'
 import footerexport from '../assets/Group 5946.png'
 import { Link } from 'react-router-dom'
+
+
+
 const Form = () => {
+  
+  let Nome= document.getElementById("nome")
+  let Email = document.getElementById("email")
+  let Senha = document.getElementById("senha")
+  let url = "./info.json"
+  fetch(url)// e utilizado para tratar arquivo
+      .then(res => res.json())
+      .then(data =>{
+        for(var Cadastro of data.Cadastrar){
+          {
+              Nome.value = Cadastro.Nome
+              Email.value = Cadastro.Email
+              Senha.value = Cadastro.Senha
+              break
+          }
+        }
+      })
+
   return (
     <div>
 
@@ -15,17 +36,17 @@ const Form = () => {
                     <h2 className='texformats'>Crie sua conta </h2>
                       <div className='faixa'>
                         <label htmlFor="nome" className='texfaixa'> Seu Nome</label>
-                        <input type="text" name='nome'  className='inpfaixa' placeholder='Exemplo: Maria da Silva'/>
+                        <input type="text" name='nome'  className='inpfaixa' placeholder='Exemplo: Maria da Silva' id='nome'/>
                       </div>
 
                       <div className='faixa'>
                         <label htmlFor="nome" className='texfaixa'> Seu Email</label>
-                        <input type="email" name='nome'  className='inpfaixa' placeholder='Exemplo: exemplo@exemplo.com.br'/>
+                        <input type="email" name='nome'  className='inpfaixa' placeholder='Exemplo: exemplo@exemplo.com.br' id='email'/>
                       </div>
 
                       <div className='faixa'>
                         <label htmlFor="nome" className='texfaixa'> Sua Senha</label>
-                        <input type="password" name='nome'  className='inpfaixa' placeholder='No mínimo 6 caracteres '/>
+                        <input type="password" name='nome'  className='inpfaixa' placeholder='No mínimo 6 caracteres '  id='senha'/>
                       </div>
 
                       <div className='btnfaixa'>                                                     
@@ -40,7 +61,9 @@ const Form = () => {
           
     </div>
     
-  )
+  ) 
+  
 }
 
-export default Form
+export default Form;
+
