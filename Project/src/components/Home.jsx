c3a80dbe5be4747995c54445f2cd5e0fa607ad76
Anselmo5 <anselmohenrique191@gmail.com>
2023-10-22 14:42:12 -0300

    update

--- Project/src/components/Home.jsx
+++ Project/src/components/Home.jsx
@@ -3,8 +3,46 @@ import "./home.css"
 import footerexport from '../assets/Group 5946.png'
 import logo from '../assets/Polygon 1.png'
 import logo2 from '../assets/pngegg 1.png'
+import Slider from 'react-slick'
+
+        
 
 const Home = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div>
       <nav>
@@ -37,6 +75,40 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+
+    {/* Cards */}
+    
+    <div className="carousel">
+      <Slider  {...settings}>
+        <div className="box">
+          <h3> 1</h3>
+        </div>
+
+        <div className="box">
+          <h3> 1</h3>
+        </div>
+
+        <div className="box">
+          <h3> 1</h3>
+        </div>
+
+
+        <div className="box">
+          <h3> 1</h3>
+        </div>
+
+        <div className="box">
+          <h3> 1</h3>
+        </div>
+
+          <div className="box">
+          <h3> 1</h3>
+        </div>
+      </Slider>
+    </div>
+
+  {/* fim cards */}
 
       <footer>
         <div className='alin'>
